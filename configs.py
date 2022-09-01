6176a7c95d2a25b8a3de57d834b57a632c62b523
Mr.Rohul <92625688+Rohul25@users.noreply.github.com>
2022-09-01 05:33:36 +0530

    Create configs.py

--- configs.py
+++ configs.py
@@ -12,6 +12,7 @@ class Config(object):
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+        FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
